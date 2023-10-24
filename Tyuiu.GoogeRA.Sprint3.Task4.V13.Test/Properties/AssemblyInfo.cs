5dcec663387855783af6875b0f309d0927866e5f
Tyuiu.GoogeRA@outlook.com <Tyuiu.GoogeRA@outlook.com>
2023-10-24 17:38:39 +0300

    Task4

---
+++ Tyuiu.GoogeRA.Sprint3.Task4.V13.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.GoogeRA.Sprint3.Task4.V13.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.GoogeRA.Sprint3.Task4.V13.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d682cd37-e8c9-4617-bd40-16b1979db557")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.GoogeRA.Sprint3.Task4.V13/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.GoogeRA.Sprint3.Task4.V13.Lib;
+
+namespace Tyuiu.GoogeRA.Sprint3.Task4.V13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #3| Выполнил :  Гооге Р.A. | ПКТб-23-1";
+            Console.WriteLine("**************************************************************");
+            Console.WriteLine("* Спринт #3                                                  *");
+            Console.WriteLine("* Тема: Использование операторов continue и break в циклах   *");
+            Console.WriteLine("* Задание #4                                                 *");
+            Console.WriteLine("* Вариант #13                                                *");
+            Console.WriteLine("* Выполнил :  Гооге Роберт Aлексндрович | ПКТб-23-1          *");
+            Console.WriteLine("**************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                   *");
+            Console.WriteLine("* На отрезке, где x принимает значения от -5 до 5,           *");
+            Console.WriteLine("* вычислить значение функции y=sin(x)/cos(x)                 *");
+            Console.WriteLine("**************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                           *");
+            Console.WriteLine("**************************************************************");
+
+            int startValue = -5;
+            int stopValue = 5;
+
+            Console.WriteLine("Старт шага " + startValue);
+            Console.WriteLine("Конец шага " + stopValue);
+
+
+            Console.WriteLine("**************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                 *");
+            Console.WriteLine("**************************************************************");
+
+            Console.WriteLine(" Сумма ряда = " + ds.Calculate(startValue, stopValue));
+            Console.ReadKey();
+        }
+    }
+}
