9f37c5e57a6e10a0a5846ee453122459f84b2759
Tyuiu.GoogeRA@outlook.com <Tyuiu.GoogeRA@outlook.com>
2023-10-24 20:15:07 +0300

    Task6

---
+++ Tyuiu.GoogeRA.Sprint3.Task6.V3.Lib/DataService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.GoogeRA.Sprint3.Task6.V3.Lib
+{
+    public class DataService : ISprint3Task6V3
+    {
+        public int GetSumTheDivisors(int startValue, int stopValue)
+        {
+            int sum = 0;
+            for (int i = startValue; i <= stopValue; i++)
+            {
+                for (int d = 1; d < 8; d++)
+                {
+                    if (i % d == 0)
+                    {
+                        sum++;
+                    }
+                }
+            }
+            return sum;
+        }
+    }
+}
---
+++ Tyuiu.GoogeRA.Sprint3.Task6.V3.Test/DataServiceTest.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+
+using Tyuiu.GoogeRA.Sprint3.Task6.V3.Lib;
+
+
+namespace Tyuiu.GoogeRA.Sprint3.Task6.V3.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int startValue = 13;
+            int stopValue = 19;
+
+            int res = ds.GetSumTheDivisors(startValue, stopValue);
+
+            int wait = 16;
+
+            Assert.AreEqual(wait, res);
+
+        }
+    }
+}
