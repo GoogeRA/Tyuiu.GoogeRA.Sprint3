47e933a263663f2d5b2e8654d5a17dd69c012b3a
Tyuiu.GoogeRA@outlook.com <Tyuiu.GoogeRA@outlook.com>
2023-10-24 21:37:14 +0300

    Task7

---
+++ Tyuiu.GoogeRA.Sprint3.Task7.V20.Lib/DataService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.GoogeRA.Sprint3.Task7.V20.Lib
+{
+    public class DataService : ISprint3Task7V20
+    {
+        public double[] GetMassFunction(int startValue, int stopValue)
+        {
+            double[] v;
+            int len = (stopValue - startValue) + 1;
+            v = new double[len];
+            double y;
+            int count = 0;
+            for (int x = startValue; x <= stopValue; x++)
+            {
+                if (x == 0)
+                {
+                    v[count] = 0.00;
+                    count++;
+                    continue;
+                }
+                y = Math.Round(((Math.Sin(x) - 2 * x) / (3 * x / 1) + Math.Sin(x) - 3 * x + 2), 2); 
+                v[count] = y;
+                count++;
+                
+            }
+            return v;
+
+        }
+    }
+}
---
+++ Tyuiu.GoogeRA.Sprint3.Task7.V20.Test/DataServiceTest.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.GoogeRA.Sprint3.Task7.V20.Lib;
+
+namespace Tyuiu.GoogeRA.Sprint3.Task7.V20.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+            DataService service1 = new DataService();
+
+            int startValue = -5;
+            int stopValue = 5;
+            int len = stopValue - startValue + 1;
+            double[] v;
+            v = new double[len];
+
+            v[0] = 17.23;
+            v[1] = 14.03;
+            v[2] = 10.21;
+            v[3] = 6.58;
+            v[4] = 3.77;
+            v[5] = 0.00;
+            v[6] = -0.54;
+            v[7] = -3.61;
+            v[8] = -7.51;
+            v[9] = -11.49;
+            v[10] = -14.69;
+
+            double[] res;
+            res = new double[len];
+            res = service1.GetMassFunction(startValue, stopValue);
+            CollectionAssert.AreEqual(v, res);
+
+        }
+    }
+}
